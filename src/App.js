abc0daed13b6e42a9fd7a33cf70cba226e1c4f87
Andrew Cathcart <andrew.cathcart@salecycle.com>
2020-08-07 15:37:12 +0100

    brief dive into various react features, inc hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,88 @@
 import React from "react";
 import "./App.css";
+import Person from "./Person/Person";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    people: [
+      { id: "dfgads13", name: "Andy", age: 25 },
+      { id: "sdfsdfg2a", name: "Bob", age: 21 },
+      { id: "dfghfgzzxc", name: "George", age: 23 },
+    ],
+    showPeople: false,
+  };
+
+  // Always update state immutably
+  deletePersonHandler = (personIndex) => {
+    const people = [...this.state.people];
+    people.splice(personIndex, 1);
+    this.setState({ people: people });
+  };
+
+  changeNameHandler = (event, id) => {
+    const personIndex = this.state.people.findIndex(
+      (person) => person.id === id
+    );
+
+    // Get inputted name
+    const person = { ...this.state.people[personIndex] };
+    person.name = event.target.value;
+
+    // Get current state and update the person object
+    const people = [...this.state.people];
+    people[personIndex] = person;
+
+    this.setState({ people: people });
+  };
+
+  togglePeopleHandler = () => {
+    const currentDisplayState = this.state.showPeople;
+    this.setState({ showPeople: !currentDisplayState });
+  };
+
+  displayPeople = () => {
+    let people = null;
+    // passing an anon function is inefficient, use bind instead if needs be
+    if (this.state.showPeople) {
+      people = (
+        <div>
+          {this.state.people.map((person, index) => {
+            return (
+              <Person
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                click={() => this.deletePersonHandler(index)}
+                change={(event) => this.changeNameHandler(event, person.id)}
+              />
+            );
+          })}
+        </div>
+      );
+    }
+
+    return people;
+  };
+
+  render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer",
+    };
+
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+        <button style={style} onClick={this.togglePeopleHandler}>
+          Toggle People
+        </button>
+        {this.displayPeople()}
+      </div>
+    );
+  }
 }
 
 export default App;
